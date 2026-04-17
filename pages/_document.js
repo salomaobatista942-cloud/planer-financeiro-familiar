066aef80b9af734e31454607bcd23346358a5cdc
salomaobatista942-cloud <salomaobatista942@gmail.com>
2026-04-17 08:31:09 -0300

    Add files via upload

---
+++ pages/_document.js
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+export default function Document() {
+  return (
+    <Html lang="pt-BR">
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
