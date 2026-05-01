e8c1e0ccf6dba69753b33cd617af48e4784881c6
salomaobatista942-cloud <salomaobatista942@gmail.com>
2026-05-01 17:19:47 -0300

    Add files via upload

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,13 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+
 export default function Document() {
   return (
     <Html lang="pt-BR">
       <Head>
-        <link href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Syne:wght@400;600;700;800&family=Space+Mono:wght@400;700&family=Inter:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
